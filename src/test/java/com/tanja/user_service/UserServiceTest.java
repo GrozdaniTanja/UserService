5a26f59dcdb56265ead5b320afbfc7e06cbb9c0b
Tanja <tanja.grozdani@student.um.si>
2024-03-14 23:59:59 +0100

    update test

--- src/test/java/com/tanja/user_service/UserServiceTest.java
+++ src/test/java/com/tanja/user_service/UserServiceTest.java
@@ -1,46 +1,37 @@
-package com.tanja.user_service;
+package com.tanja.user_service;import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import io.grpc.ManagedChannel;
-import io.grpc.ManagedChannelBuilder;
 import io.quarkus.test.junit.QuarkusTest;
-import org.junit.jupiter.api.AfterEach;
+import jakarta.inject.Inject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
+import com.tanja.user_service.model.User;
+import com.tanja.user_service.repository.UserRepository;
 @QuarkusTest
 public class UserServiceTest {
 
-    private ManagedChannel channel;
-    private UserServiceGrpc.UserServiceBlockingStub blockingStub;
+    @Inject
+    UserRepository userRepository;
+    private User user;
 
     @BeforeEach
-    public void setup() {
-        channel = ManagedChannelBuilder.forAddress("localhost", 9000)
-                .usePlaintext()
-                .build();
-        blockingStub = UserServiceGrpc.newBlockingStub(channel);
-    }
-
-    @AfterEach
-    public void tearDown() {
-        channel.shutdownNow();
+    void setUp() {
+        userRepository.deleteAll();
+        user = new User();
+        user.setFirstName("John");
+        user.setLastName("Doe");
+        user.setEmail("johndoe@example.com");
+        user.setPassword("password");
+        user.setRole("user");
+        userRepository.persist(user);
     }
 
     @Test
-    public void testCreateUser() {
-        User.CreateUserRequest request = User.CreateUserRequest.newBuilder()
-                .setFirstName("John")
-                .setLastName("Doe")
-                .setEmail("john.doe@example.com")
-                .setPassword("password")
-                .setRole("user")
-                .build();
-
-
-        User.UserResponse response = blockingStub.createUser(request);
-
-        assertNotNull(response);
-        assertNotNull(response.getId());
+    void testGetUserById() {
+        User find_user = userRepository.findById(user.getId());
+        assertEquals("John", find_user.getFirstName());
+        assertEquals("Doe", find_user.getLastName());
+        assertEquals("johndoe@example.com", find_user.getEmail());
+        assertEquals("password", find_user.getPassword());
+        assertEquals("user", find_user.getRole());
     }
 }
